f99ed8e1ce9eb12993639d964a249ead7016858f
AnnabelleEllis <ellisac@jncb.com>
2022-06-18 22:08:51 -0500

    exercise 2 completed

--- script.js
+++ script.js
@@ -23,3 +23,39 @@
 }
 
 document.getElementById("demo").innerHTML = troubleshooting();
+
+
+// ***********************************************************************
+
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ * 
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
+
+ number = Number(prompt("enter a number"));
+
+//  function numberChecker() {
+// 	 if(number === 6) {
+// 		 console.log("true");
+// 		 return true;
+// 	 } else {
+// 		console.log("false");
+// 		 return false;
+// 	 }
+//  }
+
+// Corrected Lines
+
+ function numberChecker() {
+	if(number >= 10) {
+		console.log("true");
+		return true;
+	} else {
+	   console.log("false");
+		return false;
+	}
+}
+
+ numberChecker();
