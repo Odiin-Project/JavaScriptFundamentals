dd549d024bc00aacee8a623c6f0500a41dfac192
AnnabelleEllis <ellisac@jncb.com>
2022-09-25 23:50:22 -0500

    MDN A simple Calucaltor Exercise Completed

---
+++ mdndocex/script.js
@@ -0,0 +1,31 @@
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals
+//A simple Calendar
+const select = document.querySelector('select');
+const list = document.querySelector('ul');
+const h1 = document.querySelector('h1');
+
+select.addEventListener('change', () => {
+    const choice = select.value;
+
+    // ADD CONDITIONAL HERE
+    if (choice === "February") {
+        createCalendar(28, choice);
+    } else if (choice === "January" || choice === "March" || choice === "May" || choice === "July" || choice === "August" || choice === "October" || choice === "December") {
+        createCalendar(31, choice);
+    } else {
+        createCalendar(30, choice);
+    }
+
+    createCalendar(days, choice);
+});
+
+function createCalendar(days, choice) {
+    list.innerHTML = '';
+    h1.textContent = choice;
+    for (let i = 1; i <= days; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = i;
+        list.appendChild(listItem);
+    }
+}
+
