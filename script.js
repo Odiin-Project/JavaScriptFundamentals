11afaa33dd016564db5b096f758cfea72b53e056
Annabelle Ellis <annabelleellis1999@gmail.com>
2023-01-04 21:29:55 -0500

    2 mdn loop ex completed

--- script.js
+++ script.js
@@ -1,160 +1,75 @@
 /**
- * ===== Troubleshooting =====
- * The function below should log the number 2, however it does not, 
- * see if you can fix it!
- * Be sure to fix it in the spirit of the code, do not hard code the result.
+ * ===== Launch countdown =====
+ * In this exercise, we want you to print out a simple launch countdown 
+ * to the output box, from 10 down to Blastoff.
  */
 
- function troubleshooting() {
-	const a = 1;
-	const b = 1;
+// let output = document.querySelector('.output');
+// output.innerHTML = '';
 
-	let result;
+// for(let i = 10; i >= 0; i--){
 
-	// Edit between these lines
-	// =================================
-	// result = "a" + "b";
-	// Corrected Line
-	result = a + b;
-    
-	// =================================
+// 	 const para = document.createElement('p');
+// 	if ( i === 10){
+// 		para.textContent =  `Countdown ${i}`;
+// 		output.appendChild(para);
 
-	return result;
+// 	} else if ( i === 0){
+// 		para.textContent =  `Blast Off ${i}`;
+// 		output.appendChild(para);
+
+// 	}
+// 	else{
+// 	 para.textContent = i;
+// 	 output.appendChild(para);
+// 	}
+// }
+// output.appendChild(para);
+ 
+
+// Alternative Solution
+
+const output = document.querySelector('.output');
+output.innerHTML = '';
+
+let i = 10;
+
+while (i >= 0) {
+  const para = document.createElement('p');
+  if (i === 10) {
+    para.textContent = `Countdown ${i}`;
+  } else if (i === 0) {
+    para.textContent = 'Blast off!';
+  } else {
+    para.textContent = i;
+  }
+
+  output.appendChild(para);
+
+  i--;
 }
 
-document.getElementById("demo").innerHTML = troubleshooting();
-
-
-// ***********************************************************************
 
 /**
- * The code below tells the browser to ask you for a number
- * then if that number is `6`, it returns `true` otherwise it returns `false`
- * 
- * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ * ===== Filling in a guest list =====
+ * In this exercise, we want you to take a list of names stored 
+ * in an array and put them into a guest list. But it's not quite 
+ * that easy — we don't want to let Phil and Lola in because they are 
+ * greedy and rude, and always eat all the food!
  */
 
- number = Number(prompt("enter a number"));
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-//  function numberChecker() {
-// 	 if(number === 6) {
-// 		 console.log("true");
-// 		 return true;
-// 	 } else {
-// 		console.log("false");
-// 		 return false;
-// 	 }
-//  }
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
 
-// Corrected Lines
 
- function numberChecker() {
-	if(number >= 10) {
-		console.log("true");
-		return true;
-	} else {
-	   console.log("false");
-		return false;
+for(const person of people){
+	if(person === 'Phil' || person === 'Lola'){
+		refused.textContent += `${person}, `;
+	}else{
+		admitted.textContent += `${person}, `;
 	}
 }
-
- numberChecker();
-
-// ***********************************************************************
-
-/**
-  Be sure to uncomment the items under each step below the editable section. Be sure to comment out the entire last step. If your code fails to run, or you see a "ReferenceError" in the console, make sure you have completed all of the objectives 
-  
-  Don't forget you can put "console.log" anywhere to see what your values are at any time.
-  
-  
-  
-	Step 1:
-	 Take a look at this code and try to predict what it's going to do before running it...
-	
-	After making your guess, press 'run' at the top and take a look at the output in the console. If you were surprised by anything, go back and look at the code to see what's going on.
-	
-
-
-
-	Step 2:
-	Once you have a good handle on the code snippet, delete the code then follow the following instructions between the lines and try to recreate the snippet on your own.
-	
-	---------------------------------------------------------------
-	
-	4 variables: first name, last name, current year, and birth year.
-	a 5th variable (greeting) that is constructed from the previous 4, it should contain a greeting with the person's full name and their age.
-	print the greeting with console.log
-	
-	---------------------------------------------------------------
-	
-	Once you have your version of the code working let's go back over it and edit it to make it easier to read.  After each step, make sure to run the code to make sure the code still works!
-
-
-
-
-
-	Step 3:
-	Add 2 more variables: "fullName" and "age"
-
-	Edit the greeting string to use fullName and age instead of doing the calculations in the string itself. (the greeting should look something like this: "Hello, my name is " + fullName)
-
-	Do NOT simply type the full name and age into the new variables, but set them using the calculations that were originally being done in the greeting.
-	
-	===== NOTE ====
-	In order to make the tests pass you will need to use these exact values for the years and names.  The wording needs to be exact.  If the tests fail, check spacing and punctuation:
-	
-	birthYear = 1948
-	thisYear = 1965
-	firstName = Carlos
-	lastName = Stevenson
-
-	The greeting should say "Hello! My name is Carlos Stevenson and I am 17 years old."
-*/
-
-// Edit below this line =============
-const birthYear = 1948;
-const thisYear = 1965;
-const firstName = "Carlos";
-const lastName = "Stevenson";
-const fullName = firstName + " " + lastName;
-const age = thisYear - birthYear;
-
-
-
-const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
-
-console.log(greeting);
-
-
-
-
-// Edit above this line ==============
-
-
-// Testing your code
-
-// Step 2: 
-
-// module.exports = {
-// 	testGroup: "a",
-// 	greeting,
-// 	birthYear,
-// 	thisYear,
-// 	firstName,
-// 	lastName
-// }
-
-
-// Step 3: (Be sure to comment out Step 2)
-
-// module.exports = {
-// 	testGroup: "b",
-// 	greeting,
-// 	birthYear,
-// 	thisYear,
-// 	firstName,
-// 	lastName,
-// 	fullName,
-// 	age
-// }
